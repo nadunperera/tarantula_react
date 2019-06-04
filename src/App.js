16e80760d31f192c1200f78bcac55223598de90e
Nadun Perera <nadun.perera@me.com>
2019-06-04 11:05:43 +1000

    added start pages, router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import StoreSpec from "./pages/StoreSpec";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/store" component={Store} />
+      <Route exact path="/store/spec" component={StoreSpec} />
+    </>
   );
 }
 
---
+++ src/pages/StoreSpec.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class StoreSpec extends Component {
+  state = {};
+  render() {
+    return <h1>Store Spec Page</h1>;
+  }
+}
+
+export default StoreSpec;
